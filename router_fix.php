96437601fddbb5de7377d424edc43204a21ba6dc
Michael C Stevens <mike@eighty-8.com>
2013-11-06 19:28:08 -0500

    Update router_fix.php

--- router_fix.php
+++ router_fix.php
@@ -1,11 +1,7 @@
 <?php
 
 /**
- * @module		com_grouppaybook
- * @author-name Christophe Demko
- * @adapted by  Ribamar FS
- * @copyright	Copyright (C) 2012 Christophe Demko
- * @license		GNU/GPL, see http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
  */
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
